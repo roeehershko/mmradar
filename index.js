b72ad2670082914b36a8c48328150273f39587bb
roee hershko <roee hershko>
2017-03-05 19:06:41 +0200

    test

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'twig');
-app.use("/public", express.static(__dirname + '/public'));
+app.use("/css", express.static(__dirname + '/public/css'));
+app.use("/js", express.static(__dirname + '/public/js'));
+app.use("/img", express.static(__dirname + '/public/img'));
+app.use("/fonts", express.static(__dirname + '/public/fonts'));
 
 app.get('/', function(req, res){
 
@@ -20,6 +23,13 @@ app.get('/', function(req, res){
 app.get('/post', function(req, res){
 
     res.render('post.twig', {
+        what_hello : "Hello bbb"
+    });
+});
+
+app.get('/unsubscribe', function(req, res){
+
+    res.render('unsubscribe.twig', {
         what_hello : "Hello bbb"
     });
 });
