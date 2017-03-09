f736955a48ba980a832adc952c7e0fcc688e1f8d
roee <roy@mbtechnologyil.com>
2017-03-09 16:17:04 +0200

    tesT

--- index.js
+++ index.js
@@ -7,11 +7,18 @@ var fs = require('fs');
 var mysql = require('mysql');
 var express = require('express');
 var app = express();
+var shortid = require('shortid');
+var bodyParser = require('body-parser');
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
 
 var connection = mysql.createConnection({
     host     : 'localhost',
-    user     : 'root',
-    password : '',
+    user     : 'remote',
+    password : 'Rhershko1@',
     database : 'mmradar'
 });
 
@@ -25,12 +32,44 @@ app.use("/img", express.static(__dirname + '/public/img'));
 app.use("/fonts", express.static(__dirname + '/public/fonts'));
 
 var posts = null;
-connection.query('SELECT reviews.*, offers.verified, offers.results FROM reviews INNER JOIN offers ON offers.review_id = reviews.id', [], function (err, data) {
+connection.query('SELECT reviews.*, offers.verified, offers.link, offers.results FROM reviews INNER JOIN offers ON offers.review_id = reviews.id', [], function (err, data) {
     posts = data;
 });
 
-app.get('/', function(req, res){
 
+app.use('/', function(req, res, next) {
+    var id = shortid.generate();
+    if (req.query.track) {
+        var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+        var clickData = {
+            hash: id,
+            ip: ip,
+            p1: req.query.p1 || req.query.sub || '',
+            p2: req.query.p2 || '',
+            source: req.query.source || ''
+        };
+
+        connection.query('INSERT INTO clicks SET ?', clickData, function (err, res) {
+
+            if ( ! err && req.query.sub) {
+                var subsData = {
+                    name: req.query.p2,
+                    email: req.query.sub,
+                    click_id: res.insertId
+                };
+
+                connection.query('INSERT INTO subscribers SET ? ON DUPLICATE KEY UPDATE id=id', subsData, function (err, res) {
+                    console.log(err, res);
+                });
+            }
+        });
+    }
+
+    next();
+});
+
+app.get('/', function(req, res){
     res.render('homepage.twig', {
         posts : posts
     });
@@ -43,15 +82,14 @@ app.get('/reviews/test', function(req, res){
 });
 
 app.get('/reviews/:postId', function(req, res){
-    var post = connection.query('SELECT reviews.*, offers.verified, offers.results FROM reviews INNER JOIN offers ON offers.review_id = reviews.id WHERE reviews.id = ?', [req.params.postId], function (err, data) {
+    var post = connection.query('SELECT reviews.*, offers.link, offers.verified, offers.results FROM reviews INNER JOIN offers ON offers.review_id = reviews.id WHERE reviews.id = ?', [req.params.postId], function (err, data) {
         res.render('post.twig', {
             review : data[0],
-            reviewResults: data[0].results.split(',')
+            reviewResults: data[0].results.split(','),
+            posts: posts
         });
     });
 });
-
-
 
 app.get('/unsubscribe', function(req, res){
 
@@ -61,5 +99,19 @@ app.get('/unsubscribe', function(req, res){
 });
 
 
+app.post('/subscribe', function(req, res){
+
+    var subsData = {
+        name: req.body.name || '',
+        email: req.body.email || '',
+        click_id: null
+    };
+
+    connection.query('INSERT INTO subscribers SET ? ON DUPLICATE KEY UPDATE id=id', subsData, function (err, res) {
+        console.log(err, res);
+    });
+
+    res.end('{ "success": "1" }')
+});
 
 app.listen(3000);
--- public/js/home.js
+++ public/js/home.js
@@ -6,9 +6,41 @@
         var $el = $(el);
         var offer = $el.data('review');
 
+        console.log(offer);
         $el.find('.review-box-img').css({
             'background-image': 'url(img/reviews/' + offer + '/thumb.jpg)'
         });
     });
+
+    function validateEmail(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
+
+    $('#subscribe-btn').on('click', function () {
+        var $this = $(this);
+        var $modal = $('#subscribeModal');
+        $this.attr('disabled', 'disabled');
+
+        if (validateEmail($modal.find('input[name=email]').val())) {
+            $.ajax({
+                url: '/subscribe',
+                type: 'post',
+                data: {
+                    email: $modal.find('input[name=email]').val(),
+                    name: $modal.find('input[name=name]').val(),
+                }
+            });
+
+            setTimeout(function () {
+                $modal.find('#sub-form').hide();
+                $modal.find('.hidden').removeClass('hidden');
+            }, 1500);
+        }
+        else {
+            alert('Invalid email address');
+            $this.attr('disabled', null);
+        }
+    })
 }());
 
